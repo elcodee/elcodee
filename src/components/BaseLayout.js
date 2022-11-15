d18420e086a9cb0d01feb0b29023765c9551a27e
Rama Aditya <46771792+elcodee@users.noreply.github.com>
2022-11-15 12:17:40 +0700

    Update BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -31,7 +31,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>Made With &hearts; by <a href={'https://elcodee.com'}>Elcode</a></p>
+                  <p>Made With &hearts; by <a href={'https://s.id/elcodee'}>Elcode</a></p>
                   <p>&copy; 2018 - 2022</p>
                </Box>
             </Grid>
